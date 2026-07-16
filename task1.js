8349a266dd08eb10226c2e922a74c89d3cf92110
Afra Kabir Mehita <mehitamehi2005@gmail.com>
2026-07-16 11:57:38 +0600

    Completed Task 1

---
+++ task1.js
@@ -0,0 +1,12 @@
+// Input variables
+const totalMoney = 1000;
+const orangePriceKg = 400;
+const applePriceKg = 300;
+
+// Calculate total cost
+const totalCost = orangePriceKg + applePriceKg;
+
+// Calculate change to return
+const changeReturned = totalMoney - totalCost;
+
+console.log(changeReturned);
